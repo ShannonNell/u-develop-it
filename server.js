1125d9f12a951a12d0070bb6314a132185cebf3a
Shannon <shannon.nell92@gmail.com>
2021-06-27 19:32:38 -0400

    MySQL interface working

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,8 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'dragonpower!SQL',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-//handle user requests not supported by the app
+//return data from candidates table
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
 //default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
